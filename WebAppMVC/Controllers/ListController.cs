09314b8a8b84a9d059877db8548842f98e0ddeb1
kostenkoi24 <kostenko.i@gmail.com>
2023-03-17 23:44:24 +0200

    ASPHomework1

---
+++ WebAppMVC/Controllers/ListController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMVC.Controllers
+{
+    public class ListController : Controller
+    {
+        public IActionResult Info()
+        {
+            return View();
+        }
+    }
+}
